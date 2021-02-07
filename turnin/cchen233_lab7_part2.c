ffe8391e71d8b44b09f5b6e3d55a787a93c0ff26
36tofu <christopher99chen@gmail.com>
2021-02-06 22:46:36 -0800

    parts 1-3

---
+++ turnin/cchen233_lab7_part2.c
@@ -0,0 +1,49 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	//ADMUX |= (1<<REFS0);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRD = 0xFF;
+	DDRC = 0x03;
+	DDRA = 0x00;
+	
+	PORTD = 0x00;
+	PORTC = 0x00;
+	PORTA = 0x00;
+
+	
+	unsigned short x = ADC;
+	unsigned short tmpD = 0;
+	unsigned short tmpC = 0;
+
+	ADC_init();
+
+    /* Insert your solution below */
+    while (1) {
+	x = ADC;
+	tmpD = (char)x;
+	tmpC = (char)(x>>8);
+	PORTD = tmpD;
+	//PORTD = 0;
+	//PORTC = 1;
+	PORTC = tmpC;
+    }
+    return 1;
+}
---
+++ turnin/cchen233_lab7_part3.c
@@ -0,0 +1,48 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	//ADMUX |= (1<<REFS0);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRD = 0xFF;
+	DDRC = 0x03;
+	DDRA = 0x00;
+	
+	PORTD = 0x00;
+	PORTC = 0x00;
+	PORTA = 0x00;
+
+	
+	unsigned short x = ADC;
+	unsigned short tmpD = 0;
+	unsigned short tmpC = 0;
+
+	ADC_init();
+
+    /* Insert your solution below */
+    while (1) {
+	x = ADC;
+	if(x >= 200) tmpD = 1;
+	else tmpD = 0;
+
+	PORTD = tmpD;
+	PORTC = tmpC;
+    }
+    return 1;
+}
